0041978bdbff3ca09dad9f83784638c0f7af7019
Philipp Maier <git@pmaier.at>
2022-12-09 13:41:50 +0100

    Without using math import

--- 2/main.py
+++ 2/main.py
@@ -1,13 +1,21 @@
 import math
 
 
+def product_of_list(l: list[int]) -> int:
+    sum = 1
+    for i in l:
+        sum *= i
+    return sum
+
+
 def replace_index_with_product_of_list(l: list[int]) -> list[int]:
     output = []
 
     for i in range(0, len(l)):
         before = l[:i]
         after = l[i + 1:]
-        output.append(math.prod(before + after))
+        # output.append(math.prod(before + after))
+        output.append(product_of_list(before + after))
 
     return output
 
